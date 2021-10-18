62f319f3e3f9eb69d840699230b57c2943db93bd
danieltapel <73966070+danieltapel@users.noreply.github.com>
2021-10-18 02:32:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,106 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var modeSwitch = document.querySelector('.mode-switch');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+import * as rtdb from "https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js";
+import { getAuth, signInWithRedirect, getRedirectResult, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
 
-  modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
-    modeSwitch.classList.toggle('active');
+const firebaseConfig = { //you see absolutely nothing here
+  apiKey: "AIzaSyBa2L9IA6mT7PvpTXv4vy4XCFKKMleYQI4",
+  authDomain: "ictochat-28b45.firebaseapp.com",
+  databaseURL: "https://ictochat-28b45-default-rtdb.firebaseio.com",
+  projectId: "ictochat-28b45",
+  storageBucket: "ictochat-28b45.appspot.com",
+  messagingSenderId: "913846922731",
+  appId: "1:913846922731:web:55fa87198c9f05425e3dae",
+  measurementId: "G-E9BL4YCX4S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+let username = "";
+let avatar = "";
+let email = "";
+let initialize = true;
+let db = rtdb.getDatabase(app);
+let chatRef = rtdb.ref(db, "/chats");
+let userRef = rtdb.ref(db, "/users");
+const auth = getAuth();
+
+getRedirectResult(auth)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access Google APIs.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    username = user["displayName"];
+    avatar = user["photoURL"];
+    email = user["email"];
+    document.getElementById("signIn").style.visibility = 'hidden';
+    for (let elt of document.getElementsByClassName("loggedIn")) elt.style.visibility = 'visible';
+    document.getElementById('loginText').innerHTML += username;
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
   });
-  
-  var listView = document.querySelector('.list-view');
-  var gridView = document.querySelector('.grid-view');
-  var projectsList = document.querySelector('.project-boxes');
-  
-  listView.addEventListener('click', function () {
-    gridView.classList.remove('active');
-    listView.classList.add('active');
-    projectsList.classList.remove('jsGridView');
-    projectsList.classList.add('jsListView');
-  });
-  
-  gridView.addEventListener('click', function () {
-    gridView.classList.add('active');
-    listView.classList.remove('active');
-    projectsList.classList.remove('jsListView');
-    projectsList.classList.add('jsGridView');
-  });
-  
-  document.querySelector('.messages-btn').addEventListener('click', function () {
-    document.querySelector('.messages-section').classList.add('show');
-  });
-  
-  document.querySelector('.messages-close').addEventListener('click', function() {
-    document.querySelector('.messages-section').classList.remove('show');
-  });
+
+const scrollToBottom = (node) => {
+	node.scrollTop = node.scrollHeight;
+}
+rtdb.onValue(chatRef, ss=>{
+  let obj = ss.val();
+  let backload = 0
+  if (initialize) {
+    let histLen = Object.keys(obj).length;
+    if (histLen < 30) {
+      backload = histLen - 1;
+    } else {
+      backload = 30;
+    }
+    initialize = false;  
+  }
+  for (let x = backload; x > -1; x--) {
+    let msgID = Object.keys(obj).reverse()[x];
+    let author = obj[msgID]["author"];
+    let pfp = obj[msgID]["avatar"]
+    let msg = obj[msgID]["message"];
+    let newMsg = document.createElement("div");
+    newMsg.setAttribute("class","message");
+    newMsg.setAttribute("id",msgID);
+    document.getElementById("chatBox").appendChild(newMsg);
+    document.getElementById(msgID).innerHTML += ('<div class="pfp"><img src='+pfp+' alt='+author+' style="width:32px;height:32px;"></div><div class="msgText"><p class="author">'+author+'</p><p class="message">'+msg+'</p></div>');  
+  }
+  scrollToBottom(document.getElementById('chatBox'));
 });
+
+const chatHandler = function(){
+  if (auth["currentUser"]) { //this used to scan if "username" and "avatar" had values but i changed that
+    let chat = document.querySelector("#chatInput").value.trim();
+    if (chat.length < 256 && chat) {
+      rtdb.push(chatRef, {'author':username,'message':chat,'avatar':avatar});
+    } else {
+      alert("Too long/short!");
+    }
+    document.querySelector("#chatInput").value = "";
+  } else {
+    alert("Sign in to join the conversation!");
+  }
+}
+
+const signInRedirect = function() {
+  signInWithRedirect(auth,provider); //this function frustrated me for a good 3 minutes
+}
+
+document.querySelector("#chatSubmit").addEventListener("click", chatHandler);
+document.querySelector('#chatInput').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      chatHandler();
+    }
+});
+document.querySelector("#signIn").addEventListener("click", signInRedirect);
